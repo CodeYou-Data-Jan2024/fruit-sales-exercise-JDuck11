f6d37df04913df7f4cebc9ce8594d5f512b64ea0
jduck11 <jjhowa02@icloud.com>
2024-05-19 23:02:00 -0400

    fruits

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,18 @@
 # add your code here
+import pandas as pd
+
+fruits = pd.DataFrame(columns=['Apple' , 'Bananas'])
+fruits
+
+fruits_data = {
+    "apples" :[35],
+    "Bananas" [21]   
+    }
+pd.DataFrame(fruits_data)
+
+fruits_sales - pd.DataFrame({
+    "Apples": [35,41]
+    "bananas": [21,34]}
+        index= ["2017 sales", "2018 sales"]                    
+})
+
